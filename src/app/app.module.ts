d008875c229287bc7c5daebb22cdf64e5d640304
AndreaLiCalsi <86717767+AndreaLiCalsi@users.noreply.github.com>
2021-11-03 13:14:29 +0100

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { OrderContainerComponent } from './order-container/order-container.component';
+import { OrderDetailsComponent } from './order-details/order-details.component';
+import { OrderListComponent } from './order-list/order-list.component';
+import { OrderSingleComponent } from './order-single/order-single.component';
+import { UserContainerComponent } from './user-container/user-container.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserSingleComponent } from './user-single/user-single.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrderContainerComponent,
+    OrderDetailsComponent,
+    OrderListComponent,
+    OrderSingleComponent,
+    UserContainerComponent,
+    UserDetailsComponent,
+    UserListComponent,
+    UserSingleComponent
   ],
   imports: [
     BrowserModule
